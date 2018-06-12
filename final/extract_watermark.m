0fe87c09dd1fdc3a96e6a935ef85229a0483b729
Hanbei <15510320767@163.com>
2018-06-12 11:00:02 +0800

    添加水印攻击
    不同的MP3压缩output由外部软件生成。微微调动变量使测试攻击时更简便一点。

--- final/extract_watermark.m
+++ final/extract_watermark.m
@@ -1,13 +1,32 @@
+close all
 %读取含水印音频
 n=1600; %分段长度
-[audio,fs] = audioread('output.wav');
-audio = reshape(audio,[n,length(audio)/n]);
+[output,fs] = audioread('output.wav');
+output = reshape(output,[n,length(output)/n]);
+audio = output;
 [audio1,fs1] = audioread('input.wav');   %载入原始音频，计算信噪比
 audio1 = reshape(audio1,[n,length(audio1)/n]);
 
 %%%对音频进行攻击
-%添加高斯白噪
-%audio_awng = awgn(audio,20);
+%audio = awgn(audio,20);%添加高斯白噪
+
+%audio_resample = resample(audio, fs/2, fs);
+%audio = resample(audio_resample, fs, fs/2);%重采样，先变为fs/2，再变回fs
+
+%[B1,B2]=butter(6,11025/(fs/2),'low'); %截止频率为11025Hz的低通滤波
+%audio = filter(B1,B2,audio);
+
+%[audio16bit,fs16bit] = audioread('output16bit.wav'); %重量化16bit-8bit-16bit
+%audio = reshape(audio16bit,[n,length(audio16bit)/n]);
+
+%[audio32kbs,fs32kbs] = audioread('output32kbs.mp3'); %32kbit/smp3压缩
+%audio = reshape(audio32kbs,[n/4,length(audio32kbs)/(n/4)]);
+
+%[audio64kbs,fs64kbs] = audioread('output64kbs.mp3'); %64kbit/smp3压缩
+%audio = reshape(audio64kbs,[n/2,length(audio64kbs)/(n/2)]);
+
+%[audio128kbs,fs128kbs] = audioread('output128kbs.mp3'); %128kbit/smp3压缩
+%audio = reshape(audio128kbs,[n,length(audio128kbs)/(n)]);
 
 delta = 0.5;
 len = 73*73;
@@ -29,13 +48,13 @@ watermark = reshape(watermark,[73,73]);
 imshow(watermark');
 imwrite(watermark','extract_cuc.jpg');
 
-%计算分段平均信噪比
-for j=1:length(audio)
-    if(audio(:,j)==audio1(:,j))
+%计算分段平均信噪比(有水印，未受到攻击的音频和无水印音频)
+for j=1:length(output)
+    if(output(:,j)==audio1(:,j))
         SNR(j)=100;
     else
         o = 1/n*sum((audio1(:,j)-mean(audio1(:,j))).^2);
-        d = 1/n*sum((audio1(:,j)-audio(:,j)).^2);
+        d = 1/n*sum((audio1(:,j)-output(:,j)).^2);
         SNR(j)=10*log10(o/d);
     end
 end
