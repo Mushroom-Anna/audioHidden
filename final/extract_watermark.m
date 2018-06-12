3ff75ebcba23d865ae9f4af1f5dfbb5bc6dce8a7
Hanbei <15510320767@163.com>
2018-06-12 09:16:35 +0800

    计算分段平均信噪比

--- final/extract_watermark.m
+++ final/extract_watermark.m
@@ -2,6 +2,12 @@
 n=1600; %分段长度
 [audio,fs] = audioread('output.wav');
 audio = reshape(audio,[n,length(audio)/n]);
+[audio1,fs1] = audioread('input.wav');   %载入原始音频，计算信噪比
+audio1 = reshape(audio1,[n,length(audio1)/n]);
+
+%%%对音频进行攻击
+%添加高斯白噪
+%audio_awng = awgn(audio,20);
 
 delta = 0.5;
 len = 73*73;
@@ -22,3 +28,18 @@ end
 watermark = reshape(watermark,[73,73]);
 imshow(watermark');
 imwrite(watermark','extract_cuc.jpg');
+
+%计算分段平均信噪比
+for j=1:length(audio)
+    if(audio(:,i)==audio1(:,i))
+        SNR(i)=100;
+    else
+        o = 1/n*sum((audio1(:,i)-mean(audio1(:,i))).^2);
+        d = 1/n*sum((audio1(:,i)-audio(:,i)).^2);
+        SNR(i)=10*log10(o/d);
+    end
+end
+aveSNR = 1/length(SNR)*sum(SNR)
+
+
+
